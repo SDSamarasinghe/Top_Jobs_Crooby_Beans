af4314faa7cb13a3fab3206dbdf2c19c37d1639c
SDSamarasinghe <it20605902@my.sliit.lk>
2022-03-15 18:41:36 +0530

    Add create_job ui

---
+++ app/src/main/java/com/example/topjobs/View_requested_job.java
@@ -0,0 +1,14 @@
+package com.example.topjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_requested_job extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_requested_job);
+    }
+}
