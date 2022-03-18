2116fdf823fc9d0e1eab9d6382952301cc01929e
MihisaraR99 <mihisara.r99@hotmail.com>
2022-03-18 09:48:52 +0530

    changed files and add new files

---
+++ app/src/main/java/com/example/topjobs/MySavedRequestedJobs.java
@@ -0,0 +1,14 @@
+package com.example.topjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MySavedRequestedJobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_saved_requested_jobs);
+    }
+}
